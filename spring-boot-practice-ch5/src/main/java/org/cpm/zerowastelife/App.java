16ffb2f084c3e9287fe87cc095c043082469a99c
CPM <cpm.chuck@gmail.com>
2017-10-05 22:50:04 +0800

    ch5 complete

--- spring-boot-practice-ch5/src/main/java/org/cpm/zerowastelife/App.java
+++ spring-boot-practice-ch5/src/main/java/org/cpm/zerowastelife/App.java
@@ -1,13 +1,20 @@
 package org.cpm.zerowastelife;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class App {
+	
+	@RequestMapping("/")
+	String index() {
+		return "Hello Spring Boot";
+	}
+	
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
     }
 }
